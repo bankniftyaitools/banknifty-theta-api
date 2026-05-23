0aacb7e38a4784e99b0b4dc72e92af3c9cd87dc8
bankniftyaitools <mbbchandrasekhar@gmail.com>
2026-05-23 21:06:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 
-const FYERS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOiJIWUhKMENLVEtBIiwidXVpZCI6IjYwNjlmOTQwMTlkODQ5ZmFiOWViYmFmNTNmN2Q5MDBhIiwiaXBBZGRyIjoiIiwibm9uY2UiOiIiLCJzY29wZSI6IiIsImRpc3BsYXlfbmFtZSI6IlhNMDk4OTciLCJvbXMiOiJLMSIsImhzbV9rZXkiOiIyZWFjNmFlMjcxODNhZWUzMmZlMmMzMTBmZDVlNjVkMGY2OGM4NDczN2Q0OTg1N2M0MWYzNjQ1MiIsImlzRGRwaUVuYWJsZWQiOiJOIiwiaXNNdGZFbmFibGVkIjoiWSIsImF1ZCI6IltcImQ6MVwiLFwiZDoyXCIsXCJ4OjBcIixcIng6MVwiXSIsImV4cCI6MTc3OTUwMDM1MSwiaWF0IjoxNzc5NDcwMzUxLCJpc3MiOiJhcGkubG9naW4uZnllcnMuaW4iLCJuYmYiOjE3Nzk0NzAzNTEsInN1YiI6ImF1dGhfY29kZSJ9._ctMvYY1_EfaUjS7mVavvha38gep3HoQwxNsNKf6ZK8";
+const FYERS_TOKEN = process.env.FYERS_TOKEN;
 
 app.get("/", (req, res) => {
   res.send("Bank Nifty Theta API Running 🚀");
